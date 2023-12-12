97f919dba6d816a9dee32e7899bf8c7a7a9de440
Tetiana <burlaka199327@gmail.com>
2023-12-13 00:22:32 +0200

    fix-js-4

--- js/task-1.js
+++ js/task-1.js
@@ -2,12 +2,8 @@ function isEnoughCapacity(products, containerSize) {
   let sum = 0;
   for (const product of Object.values(products)) {
     sum += product;
-    if (sum <= containerSize) {
-      return true;
-    } else {
-      return false;
-    }
   }
+  return sum <= containerSize;
 }
 
 console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
